06a8fa37eae8e5305e9f42205430aae192925568
George Lucas Gomes <gomes.georgelucas@gmail.com>
2023-01-25 13:08:24 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(new Comparator().Compare(2.3D, 2.3D));
+
+class Comparator
+{
+    public bool Compare(string objA, string objB)
+    {
+        return objA.Equals(objB);
+    }
+
+    public bool Compare(int intA, int intB)
+    {
+        return intA == intB;
+    }
+
+    public bool Compare(double doubleA, double doubleB)
+    {
+        return doubleA == doubleB;
+    }
+}
+
