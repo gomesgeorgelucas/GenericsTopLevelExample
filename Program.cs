9927c32870c714b435db3e1bffc74006e66cf0dd
George Lucas Gomes <gomes.georgelucas@gmail.com>
2023-01-25 13:18:24 -0400

    compare using generics

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
 ﻿Console.WriteLine(new Comparator().Compare(2.3D, 2.3D));
+Console.WriteLine(new Comparator().Compare(2.1D, 2.2D));
+
+Console.WriteLine(new Comparator().Compare(0.1M, 0.1M));
+Console.WriteLine(new Comparator().Compare(0.1M, 0.2M));
+
+Console.WriteLine(new Comparator().Compare("", ""));
+Console.WriteLine(new Comparator().Compare("", "2"));
+
+Console.WriteLine(new Comparator().Compare(false, false));
+Console.WriteLine(new Comparator().Compare(false, true));
+
+Console.WriteLine(new Comparator().Compare(1, 1));
+Console.WriteLine(new Comparator().Compare(1, 2));
 
 class Comparator
 {
@@ -15,6 +28,11 @@ class Comparator
     public bool Compare(double doubleA, double doubleB)
     {
         return doubleA == doubleB;
+    }
+
+    public bool Compare<T>(T genericA, T genericB) where T : IComparable<T>, IEquatable<T>
+    {
+        return genericA.Equals(genericB);
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GenericsTopLevelExample
+
+## Enunciado
+
+- Crie um overload de métodos para comparar se string, int e double são iguais.
+
+- Agora faça o mesmo que acima criando um método capaz de comparar qualquer objeto utilizando generics
